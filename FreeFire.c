8a1e3a7235bd0b9b47b8441c0b946e842fe41350
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-08 16:41:12 +0000

    Função Principal - main

--- FreeFire.c
+++ FreeFire.c
@@ -28,3 +28,36 @@ void removerItem(Item mochila[], int *total);
 void listarItens(Item mochila[], int total);
 void buscarItem(Item mochila[], int total);
 
+// Função principal
+int main() {
+    // Declaração da mochila e controle dos itens
+    Item mochila[MAX_ITENS]; 
+    int totalItens = 0;      
+    int opcao;
+
+    // Loop principal do menu
+    do {
+        printf("\n--- MENU DE INVENTARIO: FREE FIRE ---\n");
+        printf("1. Inserir Item\n");
+        printf("2. Remover Item\n");
+        printf("3. Listar Mochila\n");
+        printf("4. Buscar Item\n");
+        printf("0. Sair\n");
+        printf("Escolha uma ação: ");
+        scanf("%d", &opcao);
+        getchar(); // Limpar o buffer do teclado
+
+        // Processamento da opção escolhida utilizando switch-case
+        switch(opcao) {
+            case 1: inserirItem(mochila, &totalItens); break;
+            case 2: removerItem(mochila, &totalItens); break;
+            case 3: listarItens(mochila, totalItens); break;
+            case 4: buscarItem(mochila, totalItens); break;
+            case 0: printf("Saindo do jogo...\n"); break;
+            default: printf("Ação inválida!\n");
+        }
+    } while(opcao != 0);
+
+    return 0;
+}
+
