61ac7fa22654f071096f78f921f06c074dcd2f31
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-08 22:16:40 +0000

    PROTÓTIPOS DE FUNÇÕES

--- FreeFire.c
+++ FreeFire.c
@@ -25,3 +25,21 @@ typedef struct No {
 
 // Variáveis Globais para Contagem de Comparações
 int comparacoesBusca = 0;
+
+// --- PROTÓTIPOS DE FUNÇÕES ---
+
+// Funções de Vetor
+void inserirVetor(Item vetor[], int *totalItens);
+void listarVetor(Item vetor[], int totalItens);
+void buscarSequencialVetor(Item vetor[], int totalItens);
+void ordenarVetor(Item vetor[], int totalItens);
+void buscarBinariaVetor(Item vetor[], int totalItens);
+void removerVetor(Item vetor[], int *totalItens);
+
+// Funções de Lista Encadeada
+void inserirLista(No** primeiroItem);
+void listarLista(No* primeiroItem);
+void buscarLista(No* primeiroItem);
+void removerLista(No** primeiroItem);
+void limparMochilaLista(No* primeiroItem);
+
