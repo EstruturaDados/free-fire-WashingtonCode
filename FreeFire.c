85bf793768d083b0171d33336467314d8f14a1ae
WashingtonCode <202501483143@alunos.unifanor.edu.br>
2026-05-08 16:40:07 +0000

    Definição da struct Item

--- FreeFire.c
+++ FreeFire.c
@@ -15,3 +15,9 @@
 // Constante para definir a capacidade máxima da mochila
 #define MAX_ITENS 10
 
+// Definição da struct Item
+typedef struct {
+    char nome[30];
+    char tipo[20];
+    int quantidade;
+} Item;
